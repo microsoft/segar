b00245858668e418a6bb5a2ee330a300321c95cc
Bogdan Mazoure <bogdan.mazoure@mail.mcgill.ca>
2022-02-04 16:12:23 -0500

    added script to download runs from W&B locally

--- samples/jax/tools/download_from_wandb.py
+++ samples/jax/tools/download_from_wandb.py
@@ -2,13 +2,9 @@
 import os
 import json
 import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.ticker as ticker
-from collections import defaultdict
 
 import wandb
 import time
-import re
 import tqdm
 
 from absl import app, flags
@@ -23,29 +19,40 @@ flags.DEFINE_string("wandb_project", "dummy_project", "W&B project name")
 def main(argv):
     api = wandb.Api(timeout=19)
 
-    runs = api.runs(FLAGS.wandb_entity+"/"+FLAGS.wandb_project)
+    runs = api.runs(FLAGS.wandb_entity + "/" + FLAGS.wandb_project)
 
     for run in tqdm.tqdm(runs):
         params = json.loads(run.json_config)
-        
+
         env_name = params['env_name']['value']
         num_levels = params['num_train_levels']['value']
+        seed = params['seed']['value']
         things, difficulty, _ = env_name.split('-')
-        
+
         run_df = run.history(samples=int(1e6))
-            
+        
         columns = run_df.columns
         run_df.columns = [x.split('/')[-1] for x in columns]
-        dir_ = os.path.join('.data/')
+        dir_ = os.path.join('../data')
         if not os.path.isdir(dir_):
             os.makedirs(dir_)
-        
+
         run_df['things'] = things
         run_df['difficulty'] = difficulty
         run_df['num_levels'] = num_levels
-
-        run_df.to_csv(os.path.join(dir_,'%d.csv' %(np.random.randint(10000))))
+        run_df['seed'] = seed
+        
+        run_df.to_csv(os.path.join(dir_,
+                                   '%s_%s_%s_%d.csv' % (things, difficulty, num_levels, seed)))
+        
+        artifact = run.logged_artifacts()
+        if len(artifact):
+            artifact = artifact[0]
+            fn = artifact.file().split(os.path.sep)[-1]
+            artifact_dir = artifact.download(dir_)
+            os.rename(os.path.join(dir_, fn), os.path.join(dir_, 'checkpoint_%s_%s_%s_%d' % (things, difficulty, num_levels, seed)))
         time.sleep(3.)
+
 
 if __name__ == '__main__':
     app.run(main)
