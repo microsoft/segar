3780c63ad79079e819bb73eeb97c06e1ee36ce25
R Devon Hjelm <erroneus@gmail.com>
2022-02-08 21:06:14 -0800

    updating readme and factor inline check

--- segar/factors/factors.py
+++ segar/factors/factors.py
@@ -278,8 +278,11 @@ class FactorContainer(Factor[dict], default={}):
         if not (isinstance(value, Factor) and key.t == value.t):
             if key in self.value:
                 allow_in_place = self.value[key]._allow_in_place
-                self.value[key] = key(value)
-                self.value[key]._allow_in_place = allow_in_place
+                if allow_in_place:
+                    self.value[key] = key(value)
+                    self.value[key]._allow_in_place = allow_in_place
+                else:
+                    raise ValueError('In-place operations protected.')
             else:
                 self.value[key] = key(value)
         else:
