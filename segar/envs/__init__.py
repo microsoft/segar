c7ec43c0b2139a887d71497afe0a616bb793fb0a
Florian Golemo <fgolemo@gmail.com>
2022-02-10 15:33:32 -0500

    line length

--- segar/envs/__init__.py
+++ segar/envs/__init__.py
@@ -1,4 +1,6 @@
-__copyright__ = "Copyright (c) Microsoft Corporation and Mila - Quebec AI Institute"
+__copyright__ = (
+    "Copyright (c) Microsoft Corporation and Mila - Quebec AI Institute"
+)
 __license__ = "MIT"
 
 from segar.envs.env import SEGAREnv, SEGARSingleEnv
