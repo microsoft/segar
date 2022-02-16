c58ae5580051866ef28900f581a862a187c1c7e6
Florian Golemo <fgolemo@gmail.com>
2022-02-16 17:47:55 -0500

    pep8 fixes

--- segar/things/objects.py
+++ segar/things/objects.py
@@ -31,9 +31,7 @@ from .things import Thing
 
 
 class Object(Thing, default={Shape: Circle(0.2), Label: "object", Text: "O"}):
-    """Objects are things that can move and collide. They move over Tiles.
-
-    """
+    """Objects are things that can move and collide. They move over Tiles."""
 
     _factor_types = Thing._factor_types + (
         Collides,
@@ -53,33 +51,35 @@ class Object(Thing, default={Shape: Circle(0.2), Label: "object", Text: "O"}):
 
 
 class Ball(
-    Object, default={Shape: Circle(0.2), Mobile: True, Label: "ball", Text: "B"},
+    Object,
+    default={Shape: Circle(0.2), Mobile: True, Label: "ball", Text: "B"},
 ):
     pass
 
 
 class Magnet(
     Object,
-    default={Shape: Circle(0.2), Magnetism: -1.0, Mobile: False, Label: "magnet", Text: "T",},
+    default={Shape: Circle(0.2), Magnetism: -1.0, Mobile: False, Label: "magnet", Text: "T"},
 ):
     pass
 
 
 class Charger(
-    Object, default={Charge: -1.0, Mobile: False, Shape: Circle(0.2), Label: "charger", Text: "Q",},
+    Object,
+    default={Charge: -1.0, Mobile: False, Shape: Circle(0.2), Label: "charger", Text: "Q"},
 ):
     pass
 
 
 class Bumper(
     Object,
-    default={StoredEnergy: 1.0, Mobile: False, Shape: Circle(0.2), Label: "bumper", Text: "U",},
+    default={StoredEnergy: 1.0, Mobile: False, Shape: Circle(0.2), Label: "bumper", Text: "U"},
 ):
     pass
 
 
 class Damper(
     Object,
-    default={StoredEnergy: -0.5, Mobile: False, Shape: Circle(0.2), Label: "damper", Text: "D",},
+    default={StoredEnergy: -0.5, Mobile: False, Shape: Circle(0.2), Label: "damper", Text: "D"},
 ):
     pass
--- segar/things/tiles.py
+++ segar/things/tiles.py
@@ -22,9 +22,7 @@ from .things import Thing
 
 
 class Tile(Thing, default={Shape: Square(1.0), Label: "tile", Text: "L"}):
-    """Tiles are things that objects can move onto.
-
-    """
+    """Tiles are things that objects can move onto."""
 
     _factor_types = Thing._factor_types + (Floor, Friction, Heat)
 
@@ -41,26 +39,21 @@ class Hole(Tile, default={Shape: Circle(1.0), Label: "hole", Text: "H", Order: -
 
 
 class SandTile(Tile, default={Friction: 0.4, Shape: Square(1.0), Label: "sand", Text: "S"}):
-    """Special sand tile.
-
-    """
+    """Special sand tile."""
 
     pass
 
 
 class MagmaTile(
-    Tile, default={Heat: 1.0, Friction: 0.1, Shape: Square(1.0), Label: "magma", Text: "M",},
+    Tile,
+    default={Heat: 1.0, Friction: 0.1, Shape: Square(1.0), Label: "magma", Text: "M"},
 ):
-    """Special magma tile.
-
-    """
+    """Special magma tile."""
 
     pass
 
 
 class FireTile(MagmaTile, default={Heat: 0.5, Label: "fire", Text: "F"}):
-    """A less-intense version of magma. Has no friction by default.
-
-    """
+    """A less-intense version of magma. Has no friction by default."""
 
     pass
