9b18b3cb70f105a29779e7490de19042dc90c775
Florian Golemo <fgolemo@gmail.com>
2022-02-16 18:27:36 -0500

    fixed some commas in main sim, made sure offending env is fixed

--- samples/misc/08-break-simulator.py
+++ samples/misc/08-break-simulator.py
@@ -4,6 +4,6 @@ import segar
 
 # here's the link to the init that's responsible:
 for _ in range(100):
-    env = gym.make("Segar-tilesx1-easy-rgb-v0")
+    env = gym.make("Segar-tilesx2-hard-rgb-v0")
 
 print("this will never print")
